859fb4b6787039b3b63385668f40ace17cdaa664
Himanshu <himanshupoptani0562@gmail.com>
2021-01-23 20:26:04 +0530

    yyy

--- newsapp/views.py
+++ newsapp/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from newsapi import NewsApiClient
 
+query = ''
+
 
 def index(request):
     try:
         newsapi = NewsApiClient(api_key="d216e0827bec4110ab62ce195ed5e61e")
-        top = newsapi.get_top_headlines(q="api")
+        top = newsapi.get_top_headlines(q=query)
 
         l = top['articles']
         desc = []
@@ -19,7 +21,9 @@ def index(request):
             desc.append(f['description'])
             img.append(f['urlToImage'])
         mylist = zip(news, desc, img)
-
-        return render(request, 'index.html', context={"mylist": mylist})
+        if len(l) == 0:
+            return render(request, 'empty.html', context={"q": query})
+        else:
+            return render(request, 'index.html', context={"mylist": mylist})
     except Exception as e:
         return render(request, template_name="error.html", context={"e": e})
